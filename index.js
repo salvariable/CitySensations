ac0f5ceb3c7b5a4608656e8421fffcdeb7761bf3
Salvador <45272829+salvariable@users.noreply.github.com>
2020-08-12 09:29:11 -0500

    Add Keyboard Manager for iOS

--- index.js
+++ index.js
@@ -5,5 +5,15 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {Platform} from 'react-native';
+import KeyboardManager from 'react-native-keyboard-manager';
+
+if (Platform.OS === 'ios') {
+  KeyboardManager.setEnable(true);
+  KeyboardManager.setKeyboardDistanceFromTextField(10);
+  KeyboardManager.setPreventShowingBottomBlankSpace(true);
+  KeyboardManager.setEnableAutoToolbar(true);
+  KeyboardManager.setToolbarDoneBarButtonItemText('Done');
+}
 
 AppRegistry.registerComponent(appName, () => App);
